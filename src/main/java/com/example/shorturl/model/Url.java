73ac804cf12987068026dfbae5dd44d525b56aac
SeokCheol <lsc991123@gmail.com>
2024-05-27 19:41:27 +0900

    feat : 간단한 shorten url 개발

---
+++ src/main/java/com/example/shorturl/model/Url.java
@@ -0,0 +1,19 @@
+package com.example.shorturl.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Url {
+    @Id
+    private String id;
+    private String url;
+}
